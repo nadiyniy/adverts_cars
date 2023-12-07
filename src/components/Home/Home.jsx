28c32017282725a03905c433f2bad1c4288d7574
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-12-07 17:13:40 +0200

    add welcome page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
+// import WelcomeImage from '../../images/welcome-page-img.jpeg';
+import { Link } from 'react-router-dom';
+import { Div } from './Home.styled';
 
 const Home = () => {
 	return (
-		<div>
-			Home component
-			<p>
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem hic eveniet enim ab, accusantium alias.
-				Sed numquam ipsa iste consequatur fugit molestiae quas dolorum repellendus blanditiis repellat, impedit odio
-				repudiandae dolores delectus inventore fugiat nam nemo tempore nulla, dicta magnam eum obcaecati? Voluptatum
-				quaerat consequatur quae. Tempora, delectus deserunt, nobis facere modi ad cum accusamus fugiat similique
-				repellat esse non deleniti eligendi aliquid perferendis odio voluptates consequatur in quod. Minima, magni,
-				natus hic sint beatae officiis nobis nemo est maxime consequatur, neque tenetur quidem cumque debitis mollitia
-				assumenda! Perferendis ipsum voluptatum asperiores atque exercitationem optio voluptates blanditiis omnis nisi
-				nesciunt.
-			</p>
-		</div>
+		<Div>
+			<div className='home-background'></div>
+			<div className='home-content'>
+				<h1 className='home-title'>Welcome to RentCar</h1>
+				<h2 className='home-subtitle'>Your Gateway to Exciting Journeys</h2>
+				<p className='home-par'>
+					Welcome to [Your Company Name], where every journey is an adventure waiting to happen! We are delighted to be
+					your trusted partner in exploring the roads with comfort, style, and ease.
+				</p>
+				<h2 className='home-subtitle'>Discover Our Fleet</h2>
+				<p className='home-par'>
+					Our diverse fleet of vehicles is tailored to meet your unique travel needs. From sleek sedans to spacious SUVs
+					and stylish convertibles, we have the perfect ride for every occasion. Whether it's a family vacation, a
+					business trip, or a weekend getaway, find the ideal vehicle that suits your taste and requirements.
+				</p>
+				<Link className='home-link' to='catalog'>
+					Go rent
+				</Link>
+			</div>
+		</Div>
 	);
 };
 
