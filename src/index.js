67e3340250876bda94481c78832ff75665e13b75
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-12-08 14:41:26 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<BrowserRouter basename='/'>
+	<BrowserRouter basename='/adverts_cars/adverts_cars'>
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistor}>
 				<App />
