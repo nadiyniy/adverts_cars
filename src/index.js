95bdbe2ba1d6f68a8e6c7cd406425a08110ba79b
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-12-08 12:52:43 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { App } from './components/App';
 import { Provider } from 'react-redux';
 import { persistor, store } from './redux/store';
@@ -10,13 +10,16 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<BrowserRouter basename='/adverts_cars'>
+	// <BrowserRouter basename='/adverts_cars'>
+	<HashRouter>
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistor}>
 				<App />
 			</PersistGate>
 		</Provider>
-	</BrowserRouter>
+	</HashRouter>
+
+	// </BrowserRouter>
 );
 
 reportWebVitals();
